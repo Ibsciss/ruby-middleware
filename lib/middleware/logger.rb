d9718056a64dbabf5ff6a09eed622cfd86f34137
lilobase <alemaire@quantis.fr>
2015-11-10 14:18:04 +0100

    Add a middleware logger

---
+++ lib/middleware/logger.rb
@@ -0,0 +1,49 @@
+require 'logger'
+require 'pp'
+
+module Middleware
+
+  class Logger
+    def initialize app, logger, name = nil
+      @app = app
+      @write_to = logger
+      @middleware_name = name
+    end
+
+    def call env
+      write(
+          way_in_message(
+              next_middleware_name, env
+      ))
+
+      time = Time.now
+
+      @app.call(env).tap { |env|
+        write(
+            way_out_message(
+                next_middleware_name, (Time.now - time) * 1000.0, env
+        ))
+      }
+    end
+
+    def next_middleware_name
+      @app.class.name
+    end
+
+    def pretty_print item
+      ->(out){ PP.pp(item, out) }.('')
+    end
+
+    def way_in_message name, env
+      'Middleware %s has been called with: %s' % [name, pretty_print(env)]
+    end
+
+    def way_out_message name, time, value
+      'Middleware %s finished in %.0f ms and returns: %s' % [name, time, pretty_print(value)]
+    end
+
+    def write msg
+      @write_to.add(::Logger::INFO, msg, @middleware_name)
+    end
+  end
+end
---
+++ spec/middleware/logger_spec.rb
@@ -0,0 +1,58 @@
+require_relative '../../lib/middleware/logger'
+require 'logger'
+
+class NEXT_Middleware
+  def initialize app
+    @app = app
+  end
+
+  def call env
+    env
+  end
+end
+
+#Middleware.new('tchoutchou_orchestrator') {
+#}.inject_logger(Logger.new IO::String(''))
+
+describe Middleware::Logger do
+
+  it 'wrote the names of the next middleware in the logger' do
+    mocked_logger = instance_double('Logger');
+    expect(mocked_logger).to receive(:add).twice
+
+    described_class.new(NEXT_Middleware.new(nil), mocked_logger, 'dump_middleware').call(1)
+  end
+
+  describe '#next_middleware_name' do
+    context 'when the next middleware is a Class' do
+      it 'extracts the next middleware name from the class name' do
+        expect(described_class.new(NEXT_Middleware.new(nil), nil).next_middleware_name).to eq 'NEXT_Middleware'
+      end
+    end
+
+    context 'when the next middleware is a Lambda' do
+      it 'gives "Proc" for the next middleware' do
+        expect(described_class.new(->(env){env}, nil).next_middleware_name).to eq 'Proc'
+      end
+    end
+  end
+
+  describe '#way_out_message' do
+    it 'returns a formatted message' do
+      expect(described_class.new(nil, nil).way_out_message('Dumb', 2400, [1, 2])).to eq "Middleware Dumb finished in 2400 ms and returns: [1, 2]\n"
+    end
+  end
+
+  describe '#way_in_message' do
+    it 'returns a formatted message' do
+      expect(described_class.new(nil, nil).way_in_message('Dumb', [1, 2])).to eq "Middleware Dumb has been called with: [1, 2]\n"
+    end
+  end
+
+  describe '#pretty_print' do
+    it 'pretty prints given var' do
+      expect(described_class.new(nil, nil).pretty_print([1, 2, 3, 4, 5])).to eq "[1, 2, 3, 4, 5]\n"
+    end
+  end
+
+end
